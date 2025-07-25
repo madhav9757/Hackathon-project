70ba7a5aee8a2775700cc6585b28fbc88103e6e5
madhav9757 <madhavsemwal9@gmail.com>
2025-07-25 21:42:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center h-screen bg-gray-100 text-xl font-bold'>Hackathon</div>
+  )
+}
+
+export default App
