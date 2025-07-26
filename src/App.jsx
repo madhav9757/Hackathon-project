278b97d5f9c34217d8621181445640754f3ae6c3
madhav9757 <madhavsemwal9@gmail.com>
2025-07-26 09:43:07 +0530

    created backed and frontend folders seperate

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div className='flex items-center justify-center h-screen bg-gray-100 text-xl font-bold'>Hackathon</div>
-  )
-}
-
-export default App
